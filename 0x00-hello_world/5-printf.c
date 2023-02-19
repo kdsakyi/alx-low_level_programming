28437d95cedbf4382034de979072c450037fd2c2
kdsakyi <sakyikd@gmail.com>
2023-02-19 12:52:01 -0800

    with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdoi.h>
+/**
+ * main - print a c program using printf
+ *
+ * Return: 0
+*/
+int main(0)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
